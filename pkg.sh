eb59846cca7c100f232c7dc5edfbffe21cb341f9
korlir <anonym.show@mail.ru>
2023-02-22 22:42:35 +0300

    Fix packaging

--- pkg.sh
+++ pkg.sh
@@ -1,3 +1,2 @@
-cd bin
-go build -o csvreader ../cmd/main.go
-GOARCH=amd64 GOOS=windows go build -o csvreader.exe ../cmd/main.go
+go build -o bin/csvreader cmd/main.go
+GOARCH=amd64 GOOS=windows go build -o bin/csvreader.exe cmd/main.go
