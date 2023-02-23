f7b3d29a25d92f5dddd9111f81bfb8476bcd5821
korlir <anonym.show@mail.ru>
2023-02-23 20:25:35 +0300

    Add file opening, parsing csv and error handling

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,33 @@
 package main
+
 import (
 	"fmt"
+	"os"
+	"test_task/internal/parsing"
 )
 
 func main() {
-	fmt.Println("h")
+	if len(os.Args) == 1 {
+		fmt.Println("Введите название .csv файла, который нужно обработать")
+		return
+	} else if len(os.Args) > 2 {
+		fmt.Println("Слишком много аргументов. Укажите ровно один .csv файл")
+		return
+	}
+	file, err := os.Open(os.Args[1])
+
+	if err != nil {
+		fmt.Println("Не удалось открыть файл. Ошибка: "+err.Error())
+		return
+	}
+	defer file.Close()
+
+	csv, err := parsing.ParseCsv(file)
+
+	if err != nil {
+		fmt.Println("Ошибка при парсинге CSV: "+err.Error())
+		return
+	}
+	csv.Print()
+	
 }
