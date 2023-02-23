79c9c9f35b835875c7452c02cb90c682c984b95b
korlir <anonym.show@mail.ru>
2023-02-24 01:04:27 +0300

    Add operations that allowed in csv

---
+++ internal/calculating/csv_operation_test.go
@@ -0,0 +1,29 @@
+package calculating
+import (
+	"testing"
+)
+
+func TestOpeartions(t *testing.T) {
+	t.Log("Start check operations.")
+	{
+		testID := 0
+
+		t.Logf("\tTest %d: check division by zero", testID)
+		{
+			_, err := AllowedOperations["/"](2, 0)
+
+			if err == nil {
+				t.Logf("\tFail on test %d. Expected error but nothing was got", testID)
+				t.Fail()
+			}
+
+			if _, ok := err.(CalculatingError); !ok {
+				t.Logf("\tFail on test %d. Expected another error but found "+err.Error(), testID)
+				t.Fail()
+			}
+		}
+		testID++
+
+		// другие тесты для будущих операций...
+	}
+}
---
+++ internal/calculating/csv_operations.go
@@ -0,0 +1,30 @@
+package calculating
+
+import "errors"
+
+type CalculatingError error
+
+var AllowedOperations = map[string]Operation{
+	"+": add, "-": sub, "*": mul, "/": div,
+}
+
+type Operation func(int, int) (int, error)
+
+func add (a int, b int) (int, error) {
+	return a+b, nil
+}
+
+func sub (a int, b int) (int, error) {
+	return a-b, nil
+}
+
+func mul (a int, b int) (int, error) {
+	return a*b, nil
+}
+
+func div (a int, b int) (int, error) {
+	if b == 0 {
+		return 0, errors.New("деление на ноль невозможно")
+	}
+	return a/b, nil
+}
