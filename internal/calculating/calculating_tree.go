a82c52618c72afbc3499053fd0acf7b45f0f20e6
korlir <anonym.show@mail.ru>
2023-02-25 03:06:36 +0300

    Add tree creating and unit tests for it

---
+++ internal/calculating/calculating_tree.go
@@ -0,0 +1,66 @@
+package calculating
+
+import (
+	"errors"
+	"strconv"
+	"test_task/internal/format"
+	"test_task/internal/operations"
+	"test_task/internal/parsing"
+)
+
+type TreeCreatingError error
+type calculatingTree map[string][]string
+
+func CreateTree(csv format.Csv) (calculatingTree, error) {
+	tree := make(map[string][]string, 0)
+	for index, line := range csv.Data {
+		for jndex, cell := range line {
+			if parsing.IsFormula(cell) {
+				link, err := csv.GetLinkByIndexes(jndex, index)
+				if err != nil {
+					panic(err)
+				}
+				formula, err := parsing.ParseCell(cell)
+				if err != nil {
+					return calculatingTree{}, err
+				}
+
+				// если формулу можно посчитать сразу, то вычисляем на месте
+				if !formula.FirstOperand.IsLink() && !formula.SecondOperand.IsLink() {
+					value, err := formula.Action(formula.FirstOperand.GetConstant(),
+						formula.SecondOperand.GetConstant())
+
+					if err != nil {
+						return calculatingTree{}, err.(operations.CalculatingError)
+					}
+					csv.Data[index][jndex] = strconv.FormatInt(int64(value), 10)
+					continue
+				}
+
+				if _, ok := tree[link]; !ok {
+					tree[link] = make([]string, 0)
+				}
+
+				if formula.FirstOperand.IsLink() {
+					if !csv.IsLinkExist(formula.FirstOperand.GetLink()) {
+						return calculatingTree{},
+							errors.New("ячейки " + formula.FirstOperand.GetLink() + " не существует").(TreeCreatingError)
+					}
+
+					tree[link] = append(tree[link], formula.FirstOperand.GetLink())
+				}
+
+				if formula.SecondOperand.IsLink() {
+					if !csv.IsLinkExist(formula.SecondOperand.GetLink()) {
+						return calculatingTree{},
+							errors.New("ячейки " + formula.SecondOperand.GetLink() + " не существует").(TreeCreatingError)
+					}
+
+					tree[link] = append(tree[link], formula.SecondOperand.GetLink())
+				}
+			}
+		}
+	}
+
+	return tree, nil
+}
---
+++ internal/calculating/calculating_tree_test.go
@@ -0,0 +1,132 @@
+package calculating
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+	"test_task/internal/parsing"
+	"testing"
+)
+
+func TestCalculatingTreeCreating(t *testing.T) {
+	t.Log("Start check creating calculating graph.")
+	{
+		testID := 0
+
+		t.Logf("\tTest %d: check calculating formulas without links", testID)
+		{
+			reader := strings.NewReader(",A,B,Cell\n1,1,0,1\n2,2,=2*2,0\n30,0,=4/2,5")
+
+			csv, err := parsing.ParseCsv(reader)
+
+			if err != nil {
+				t.Logf("\tFail on test %d. Found error after parsing: "+err.Error(), testID)
+				t.Fail()
+			}
+
+			_, err = CreateTree(csv)
+
+			if err != nil {
+				t.Logf("\tFail on test %d. Found error after calculation: "+err.Error(), testID)
+				t.Fail()
+			}
+
+			buf := bytes.NewBufferString("")
+
+			csv.PrintWithWriter(buf)
+
+			if buf.String() != ",A,B,Cell\n1,1,0,1\n2,2,4,0\n30,0,2,5\n" {
+				t.Logf("\tFail on test %d. Expected another output but got "+buf.String(), testID)
+				t.Fail()
+			}
+		}
+		testID++
+
+		t.Logf("\tTest %d: check creating graph from example csv", testID)
+		{
+			reader := strings.NewReader(",A,B,Cell\n1,1,0,1\n2,2,=A1+Cell30,0\n30,0,=B1+A1,5")
+
+			csv, err := parsing.ParseCsv(reader)
+
+			if err != nil {
+				t.Logf("\tFail on test %d. Found error after parsing: "+err.Error(), testID)
+				t.Fail()
+			}
+
+			tree, err := CreateTree(csv)
+
+			if err != nil {
+				t.Logf("\tFail on test %d. Found error after tree creating: "+err.Error(), testID)
+				t.Fail()
+			}
+
+			// reflect.DeepEqual не справился..
+			if fmt.Sprint(tree) != fmt.Sprint(map[string][]string{"B2": {"A1", "Cell30"}, "B30": {"B1", "A1"}}) {
+				t.Logf("\tFail on test %d. Expected another tree but found: %q", testID, tree)
+				t.Fail()
+			}
+		}
+		testID++
+
+		t.Logf("\tTest %d: check creating graph with not existing row", testID)
+		{
+			reader := strings.NewReader(",A,B,Cell\n1,1,0,1\n2,2,=A1+Cell40,0\n30,0,=B1+A1,5")
+
+			csv, err := parsing.ParseCsv(reader)
+
+			if err != nil {
+				t.Logf("\tFail on test %d. Found error after parsing: "+err.Error(), testID)
+				t.Fail()
+			}
+
+			_, err = CreateTree(csv)
+
+			if err == nil {
+				t.Logf("\tFail on test %d. Expected error but nothing got", testID)
+				t.Fail()
+			}
+		}
+		testID++
+
+		t.Logf("\tTest %d: check creating graph with not existing col", testID)
+		{
+			reader := strings.NewReader(",A,B,Cell\n1,1,0,1\n2,2,=A1+Cells30,0\n30,0,=B1+A1,5")
+
+			csv, err := parsing.ParseCsv(reader)
+
+			if err != nil {
+				t.Logf("\tFail on test %d. Found error after parsing: "+err.Error(), testID)
+				t.Fail()
+			}
+
+			_, err = CreateTree(csv)
+
+			if err == nil {
+				t.Logf("\tFail on test %d. Expected error but nothing got", testID)
+				t.Fail()
+			}
+		}
+		testID++
+
+		t.Logf("\tTest %d: check creating graph with zero division", testID)
+		{
+			reader := strings.NewReader(",A,B,Cell\n1,1,0,1\n2,2,=5/0,0\n30,0,=B1+A1,5")
+
+			csv, err := parsing.ParseCsv(reader)
+
+			if err != nil {
+				t.Logf("\tFail on test %d. Found error after parsing: "+err.Error(), testID)
+				t.Fail()
+			}
+
+			_, err = CreateTree(csv)
+
+			if err == nil {
+				t.Logf("\tFail on test %d. Expected error but nothing got", testID)
+				t.Fail()
+			}
+		}
+		testID++
+
+	}
+}
